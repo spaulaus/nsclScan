866dca9ee4454551c3d769c421167bfc037aeb3c
S.V. Paulauskas <stanpaulauskas@gmail.com>
2014-01-15 22:44:24 -0500

    Initial Commit

---
+++ Makefile
@@ -0,0 +1,52 @@
+#!/bin/make
+#SHELL=/bin/bash
+
+#Define the virtual paths
+vpath %.cpp src/
+vpath %.hpp inc/
+vpath %.o obj/
+
+#Set some of the compile options
+CXX = g++
+CXXFLAGS = -fPIC -g -std=c++11 -Wall $(CINCLUDEDIRS)
+LDLIBS = -ldl -lpthread -L/mnt/analysis/e13504/svp/ddasdumper -lddaschannel
+CINCLUDEDIRS = -Iinc
+c++SrcSuf = cpp
+
+#Set the name of the program to be compiled
+PROGRAM = ex
+
+#Define Objects
+TESTO = ex.o
+
+#Make the object list and prefix the object directory
+OBJS = $(TESTO)
+OBJDIR = obj
+OBJS_W_DIR = $(addprefix $(OBJDIR)/,$(OBJS))
+
+#Add the ROOT config stuff to the compilation
+ROOTCONFIG   := root-config
+CXXFLAGS     += $(shell $(ROOTCONFIG) --cflags)
+LDFLAGS      += $(shell $(ROOTCONFIG) --ldflags)
+LDLIBS       += $(shell $(ROOTCONFIG) --libs)
+#LDLIBS       += $(shell $(ROOTCONFIG) --libs) -lRooFit -lRooFitCore
+
+.SUFFIXES: .$(c++SrcSuf)
+
+all: $(OBJS_W_DIR) $(PROGRAM)
+
+$(OBJS_W_DIR): | $(OBJDIR)
+
+$(OBJDIR):
+	mkdir $(OBJDIR)
+
+$(PROGRAM): $(OBJS_W_DIR)
+	$(CXX) $(CXXFLAGS) $(LDLIBS) $^ -o $@
+
+$(OBJDIR)/%.o: %.cpp
+	$(CXX) $(CXXFLAGS) -c $< -o $@
+
+.PHONY: clean
+clean: 
+	@echo "Cleaning..."
+	@rm -f $(OBJDIR)/* $(PROGRAM) *~ src/*~ include/*~
---
+++ src/ex.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+
+#include <TTree.h>
+#include <TFile.h>
+
+#include "DDASEvent.h"
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+    cout << "Testing this shit" << endl;
+
+    TFile file("/mnt/analysis/e13504/svp/rootFiles/test00.root");
+    TTree *tr = (TTree*)file.Get("dchan");
+    TBranch *br = tr->FindBranch("ddasevent");
+
+    DDASEvent *event = new DDASEvent(); 
+    br->SetAddress(&event);
+
+    int numEvent = tr->GetEntries();
+    cout << numEvent << endl;
+
+    for (int i = 0; i < numEvent; i++) {
+        br->GetEntry(i); //Read complete accepted event in memory.
+        vector<ddaschannel*> evt = event->GetData();
+        if(evt.size() > 2) {
+            cout << "Reading event Number " << i << endl
+                 << "------------------------------" << endl;
+            
+            cout << "Boss, there are " << evt.size() << " events in this bitch."
+                 << endl;
+            for(auto j : evt) {
+                unsigned int id = j->GetID();
+                unsigned int sId  = j->GetSlotID();
+                unsigned int crId = j->GetCrateID();
+                unsigned int chId = j->GetChannelID();
+                unsigned int en = j->GetEnergy();
+                cout << id << " " << sId << " " << crId << " " 
+                     << chId << " " << en << endl;
+            }
+            cout << endl << endl;
+        }
+    }
+    //Delete ClonesArray and histogram objects
+    //event->Finish();
+}
